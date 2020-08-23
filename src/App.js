444f5fd7d0184aa57b31d8c5faec027e38b33000
Valentina <61659523+deminavalentina22@users.noreply.github.com>
2020-08-23 11:41:20 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -2,25 +2,47 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const day = '23.08.2020';
+const today = `Today is ${day}`;
+const tuesday = true;
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+				<p style={{
+					color: 'pink',
+					fontSize: 35
+				}}>
+					Hey todo mundo!
+				</p>
+				<p>
+					{today}
+				</p>
+				<p>{23 + 8}
+				</p>
+				<p>
+					{tuesday ? `Yeeeees!` : `No!`}
+					{undefined}
+					{null}
+					{true}
+					{false}
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
         </a>
-      </header>
-    </div>
-  );
+			</header>
+		</div>
+	);
 }
 
 export default App;
